5325bdbdf02c425e65a8ed5310b2f76a3f31392f
smailtaibii78-a11y <smailtaibii78@gmail.com>
2026-03-04 14:04:25 -0800

    Add incidence matrix builder

--- network/network.py
+++ network/network.py
@@ -18,3 +18,27 @@ class Network:
             f"Network(Nodes={len(self.nodes)}, "
             f"Pipes={len(self.pipes)})"
         )
+       def build_incidence_matrix(self):
+        """
+        Builds the node-pipe incidence matrix.
+        """
+        import numpy as np
+
+        num_nodes = len(self.nodes)
+        num_pipes = len(self.pipes)
+
+        A = np.zeros((num_nodes, num_pipes))
+
+        node_ids = list(self.nodes.keys())
+        pipe_ids = list(self.pipes.keys())
+
+        for j, pipe_id in enumerate(pipe_ids):
+            pipe = self.pipes[pipe_id]
+
+            start_index = node_ids.index(pipe.start_node)
+            end_index = node_ids.index(pipe.end_node)
+
+            A[start_index, j] = -1
+            A[end_index, j] = 1
+
+        return A
