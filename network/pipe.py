5411074ab164aa2ecee3dcdc31130c409199d2b7
smailtaibii78-a11y <smailtaibii78@gmail.com>
2026-03-04 16:09:21 -0800

    Update pipe.py

--- network/pipe.py
+++ network/pipe.py
@@ -3,13 +3,13 @@ class Pipe:
     Represents a pipe connecting two nodes in the network.
     """
 
-    p1 = Pipe(1, start_node=1, end_node=2, length=500, diameter=0.3, roughness=0.0001)
+    p1 = Pipe(1, start_node=1, end_node=2, length=500, diameter=0.3, roughness=0.001)
         self.id = pipe_id
         self.start_node = 1
         self.end_node = 2
         self.length = 500
         self.diameter = 0.3
-        self.roughness = 0.0001
+        self.roughness = 0.001
 
         p1 = Pipe(1, start_node=1, end_node=2, length=500, diameter=0.3)
         self.start_node.add_pipe(self)
